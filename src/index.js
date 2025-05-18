01b4965eab8d7c0ca5989f2eb35536b284b1e893
ayush969696 <ayush972982@gmail.com>
2025-05-18 11:47:09 +0530

    adding dummy

--- src/index.js
+++ src/index.js
@@ -10,11 +10,13 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-app.get('/',(req,res)=>{
-    res.send('API is running');
-})
-
-// Server Start 
-app.listen(port,()=>{
-    console.log(`Server running on port ${port}`);
+app.get("/", (req, res) => {
+  res.send("API is running");
 });
+
+// Server Start
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+// just testing git
